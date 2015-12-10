7011e7898d209a84a41ac3da64f28da13e01af74
LaughingMaan <laughingmaan@icloud.com>
2015-12-10 14:54:23 +0300

    Minor code fixes

--- Serialization/src/ru/lg/laughingman/Main.java
+++ Serialization/src/ru/lg/laughingman/Main.java
@@ -4,6 +4,45 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import by.zti.SerializationManager;
 
+/*private static void serData(String file_name, Object obj) {
+try {
+	FileOutputStream fileOut = new FileOutputStream(file_name + ".ser");
+	ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	out.writeObject(obj);
+	fileOut.close();
+	out.close();
+
+} catch (FileNotFoundException ex) {
+	System.out.println("File not found!");
+	System.exit(1);
+} catch (IOException ex) {
+	System.out.println("I/O error");
+	System.exit(2);
+}
+
+}
+
+private static Object deserData(String file_name) {
+Object retObject = null;
+try {
+	FileInputStream fileIn = new FileInputStream(file_name + ".ser");
+	ObjectInputStream in = new ObjectInputStream(fileIn);
+	retObject = in.readObject();
+	fileIn.close();
+	in.close();
+} catch (FileNotFoundException ex) {
+	System.out.println("File not found!");
+	System.exit(1);
+} catch (IOException ex) {
+	System.out.println("I/O error");
+	System.exit(2);
+} catch (ClassNotFoundException e) {
+	System.out.println("Class not found");
+	System.exit(3);
+}
+return retObject;
+}*/
+
 public class Main {
 	private static ArrayList<Profile> profiles = new ArrayList<Profile>();
 
@@ -21,44 +60,4 @@ public class Main {
 		System.out.println(profiles.size());
 		SerializationManager.serializeData(profiles, "Profiles", "ser", "");
 	}
-
-	/*private static void serData(String file_name, Object obj) {
-		try {
-			FileOutputStream fileOut = new FileOutputStream(file_name + ".ser");
-			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(obj);
-			fileOut.close();
-			out.close();
-
-		} catch (FileNotFoundException ex) {
-			System.out.println("File not found!");
-			System.exit(1);
-		} catch (IOException ex) {
-			System.out.println("I/O error");
-			System.exit(2);
-		}
-
-	}
-
-	private static Object deserData(String file_name) {
-		Object retObject = null;
-		try {
-			FileInputStream fileIn = new FileInputStream(file_name + ".ser");
-			ObjectInputStream in = new ObjectInputStream(fileIn);
-			retObject = in.readObject();
-			fileIn.close();
-			in.close();
-		} catch (FileNotFoundException ex) {
-			System.out.println("File not found!");
-			System.exit(1);
-		} catch (IOException ex) {
-			System.out.println("I/O error");
-			System.exit(2);
-		} catch (ClassNotFoundException e) {
-			System.out.println("Class not found");
-			System.exit(3);
-		}
-		return retObject;
-	}*/
-
 }
